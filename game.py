6b59bc5ceb51d25dbb964ff7f5fda9719552abff
janani <jananim118@gmail.com>
2025-07-04 14:23:02 +0530

    Add CLI Rock Paper Scissors game

---
+++ game.py
@@ -0,0 +1,20 @@
+import random
+
+def play():
+    choices = ['rock', 'paper', 'scissors']
+    user = input("Choose rock, paper or scissors: ").lower()
+    computer = random.choice(choices)
+
+    print(f"You chose {user}, computer chose {computer}")
+
+    if user == computer:
+        print("It's a tie!")
+    elif (user == "rock" and computer == "scissors") or \
+         (user == "paper" and computer == "rock") or \
+         (user == "scissors" and computer == "paper"):
+        print("You win!")
+    else:
+        print("Computer wins!")
+
+if __name__ == "__main__":
+    play()
